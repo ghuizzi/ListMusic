8a593998bedc7b5565b57acc217f8124f98c84c6
Gian vespa <gian@vantageio.com>
2022-06-12 17:30:10 -0500

    refactor: file app, styles

--- src/NavBar/Nav.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../Styles/navBar.css";
-
-const Nav = () => {
-  return <div className="navContent">Nav</div>;
-};
-
-export default Nav;
---
+++ src/components/NavBar/Nav.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+// import "../Styles/navBar.css";
+
+const Nav = () => {
+  return <div>Nav</div>;
+};
+
+export default Nav;
