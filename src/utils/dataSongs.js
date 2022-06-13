b7c663e2895352e786099961d46e32ce1cc494d0
Gian vespa <gian@vantageio.com>
2022-06-12 22:22:10 -0500

    feat: data song

---
+++ src/utils/dataSongs.js
@@ -0,0 +1,89 @@
+const Artists = [
+  // miley
+  {
+    name: 'miley cyrus',
+    albums: [
+      {
+        name: 'My album',
+        picture: require('../image/mc.jpg'),
+        age: 1,
+        description: 'description',
+        music: [
+          {
+            name: '',
+            duration: '',
+          },
+          {
+            name: '',
+            duration: '',
+          },
+          {
+            name: '',
+            duration: '',
+          },
+          {
+            name: '',
+            duration: '',
+          },
+        ],
+      },
+      {
+        name: 'Done',
+        picture: require('../image/mc.jpg'),
+        age: 1,
+        description: '',
+        music: [
+          {
+            name: '',
+            duration: '',
+          },
+          {
+            name: '',
+            duration: '',
+          },
+          {
+            name: '',
+            duration: '',
+          },
+          {
+            name: '',
+            duration: '',
+          },
+        ],
+      },
+    ],
+  },
+
+  // Bad Bunny
+  {
+    name: 'Bad Bunny',
+    albums: [
+      {
+        name: 'My album',
+        picture: require('../image/bb.jpg'),
+        age: 1,
+        description: 'description',
+        music: [
+          {
+            name: '',
+            duration: '',
+          },
+          {
+            name: '',
+            duration: '',
+          },
+          {
+            name: '',
+            duration: '',
+          },
+          {
+            name: '',
+            duration: '',
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export default Artists;
