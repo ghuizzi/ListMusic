af835b31ef462a6cf1ae1b76d705bfaf021a7029
Gian vespa <gian@vantageio.com>
2022-06-12 22:23:30 -0500

    refactor: dashboar

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,104 +1,23 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import dataSongs from "../dataSongs";
-import mc from "../image/mc.jpg";
-import "../Styles/Dash.css";
-import "../Styles/App.css";
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+import { Album } from '../components/Album';
+import { dataArtists } from '../utils';
+
+import '../styles/App.css';
+
 function Dashboard() {
   return (
     <div className="bg-gradient">
       <Container fluid className="py-4 px-3">
         <Row>
-          <Col>
-            <div className="songsContent">
-              <div className="SongsCard">
-                <div className="cardEffect">
-                  <div className="card">
-                    <img
-                      className="img-fluid rounded float-start"
-                      src={mc}
-                      alt="album"
-                    />
-                    <div className="cardDescription">
-                      <p>Artist: {Songs.name}</p>
-                      <p>Song name{Songs.Album}</p>
-                      <p> Date:{Songs.Date}</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="cardEffect">
-                  <div className="card">
-                    <img
-                      className="img-fluid rounded float-start"
-                      src={Songs.pic}
-                      alt="album"
-                    />
-                    <div className="cardDescription">
-                      <p>Artist:{Songs.name}</p>
-                      <p>Song name{Songs.Album}</p>
-                      <p>Album{Songs.Date}</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="cardEffect">
-                  <div className="card">
-                    <img
-                      className="img-fluid rounded float-start"
-                      src={mc}
-                      alt="album"
-                    />
-                    <div className="cardDescription">
-                      <p>Artist{Songs.name}</p>
-                      <p>Song name{Songs.Album}</p>
-                      <p>Album{Songs.Date}</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="cardEffect">
-                  <div className="card">
-                    <img
-                      className="img-fluid rounded float-start"
-                      src={mc}
-                      alt="album"
-                    />
-                    <div className="cardDescription">
-                      <p>{Songs.name}</p>
-                      <p>{Songs.Album}</p>
-                      <p>{Songs.Date}</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="cardEffect">
-                  <div className="card">
-                    <img
-                      className="img-fluid rounded float-start"
-                      src={mc}
-                      alt="album"
-                    />
-                    <div className="cardDescription">
-                      <p>{Songs.name}</p>
-                      <p>{Songs.Album}</p>
-                      <p>{Songs.Date}</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="cardEffect">
-                  <div className="card">
-                    <img
-                      className="img-fluid rounded float-start"
-                      src={mc}
-                      alt="album"
-                    />
-                    <div className="cardDescription">
-                      <p>{Songs.name}</p>
-                      <p>{Songs.Album}</p>
-                      <p>{Songs.Date}</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </Col>
+          {dataArtists.map((artist) =>
+            artist.albums.map((album, index) => (
+              <Col key={index} sm={12} md={6} lg={4} className="mb-4">
+                <Album album={album} artist={artist.name} />
+              </Col>
+            ))
+          )}
         </Row>
       </Container>
     </div>
