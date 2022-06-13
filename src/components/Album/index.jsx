215ac10cbdc71d0501853f601a5cd6d7d1e33266
Gian vespa <gian@vantageio.com>
2022-06-12 23:21:18 -0500

    new component modal list music

--- src/components/Album/index.jsx
+++ src/components/Album/index.jsx
@@ -1,30 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { ListMusicModal } from './modal';
 import './style.css';
 
 export function Album({ album, artist }) {
+  const [modalShow, setModalShow] = useState(false);
+
   const handleClick = () => {
-    console.log(album.music);
+    setModalShow(true);
   };
 
   return (
-    <div className="songsContent" onClick={handleClick}>
-      <div className="SongsCard">
-        <div className="cardEffect">
-          <div className="card">
-            <img
-              className="img-fluid rounded float-start"
-              src={album.picture}
-              alt="album"
-            />
-            <div className="cardDescription">
-              <p>Artist: {artist}</p>
-              <p>Album: {album.name}</p>
-              <p>Description: {album.description}</p>
-              <p>Date: {album.age}</p>
-            </div>
-          </div>
+    <>
+      <ListMusicModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        artist={artist}
+        album={album.name}
+        music={album.music}
+      />
+
+      <div className="card" onClick={handleClick}>
+        <img
+          className="img-fluid rounded float-start"
+          src={album.picture}
+          alt="album"
+        />
+        <div className="cardDescription">
+          <p>Artist: {artist}</p>
+          <p>Album: {album.name}</p>
+          <p>Description: {album.description}</p>
+          <p>Date: {album.age}</p>
         </div>
       </div>
-    </div>
+    </>
   );
 }
