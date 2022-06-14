1791fa3805b447bea6e133f3e89c57e5fc540351
Gian vespa <gian@vantageio.com>
2022-06-13 19:29:26 -0500

    use demo icon

--- src/components/Album/modal/index.jsx
+++ src/components/Album/modal/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Modal, Button } from 'react-bootstrap';
 
+// pack icons
+import { AiTwotoneStar } from 'react-icons/ai';
+
 export function ListMusicModal({ show, onHide, artist, album, music }) {
   return (
     <Modal show={show} onHide={onHide} backdrop="static" size="lg" centered>
@@ -13,6 +16,8 @@ export function ListMusicModal({ show, onHide, artist, album, music }) {
           music.map((music, index) => (
             <div key={index}>
               <p>{index + 1}</p>
+              {/* use icon */}
+              <AiTwotoneStar color="#e323a6" />
               <p>{music.name}</p>
             </div>
           ))}
