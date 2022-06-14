c23bcff59d19ef4a61980b975f6b783f6a902a78
Gian vespa <gian@vantageio.com>
2022-06-13 22:02:06 -0500

    feat: show name routes in navBar

--- src/components/NavBar/Nav.jsx
+++ src/components/NavBar/Nav.jsx
@@ -1,14 +1,27 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import "./navBar.css";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
+import { routes } from '../../router/routes';
+import { Capitalize } from '../../utils';
+
+import './navBar.css';
 const Nav = () => {
   return (
     <div classname="navContent">
       <Container>
         <Row>
           <Col>
-            {" "}
+            {routes[0].children.map((route, index) => (
+              <NavLink
+                key={index}
+                to={route.path}
+                // className="nav-link"
+                activeClassName="active"
+              >
+                {route.path === '' ? 'Home' : Capitalize(route.path)}
+              </NavLink>
+            ))}
             <a>Favorites</a>
           </Col>
         </Row>
